9fb34c4b54a76bb4128b0bfa70ff9f683899b7cd
bobbylemm <chidozienwoga@gmail.com>
2019-03-05 12:06:43 +0100

    chore: setup empty react project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AppComp = () => {
+  return (
+    <div>
+      <h2>this is indiana front-end pages kelvin</h2>
+    </div>
+  );
+};
+export default AppComp;
---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src", "index.js")
+  },
+  output: {
+    path: path.join(__dirname, "public", "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  resolve: { extensions: [".js", ".jsx"] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true
+            }
+          },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
