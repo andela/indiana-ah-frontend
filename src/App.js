7d8e434f167ebf966ba26a32e9120e1fa319d9ce
Akeem Balogun <balogunbiola101@gmail.com>
2019-03-05 17:05:42 +0100

    chore: implement react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const AppComp = () => (
   <Provider store={store}>
+  <BrowserRouter>
     <div>
       <h2>Welcome to Author's Haven</h2>
     </div>
+    </BrowserRouter>
   </Provider>
 );
 export default AppComp;
