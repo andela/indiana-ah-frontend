766904c942ae60b446815645056e889532bfaaea
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-03-05 14:58:04 +0100

    chore: setup redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const AppComp = () => {
-  return (
+const AppComp = () => (
+  <Provider store={store}>
     <div>
       <h2>this is indiana front-end pages kelvin</h2>
     </div>
-  );
-};
+  </Provider>
+);
 export default AppComp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppComp from "./App";
-import "./styles/styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComp from './App';
+import './styles/styles.scss';
 
-ReactDOM.render(<AppComp />, document.getElementById("root"));
+ReactDOM.render(<AppComp />, document.getElementById('root'));
