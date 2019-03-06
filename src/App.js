3689f7fb79a56f02f9d9b529f8b71f4f838039e0
Akeem Balogun <balogunbiola101@gmail.com>
2019-03-06 09:41:20 +0100

    chore: change app name

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-const AppComp = () => (
+const App = () => (
   <Provider store={store}>
   <BrowserRouter>
-    <div>
-      <h2>Welcome to Author's Haven</h2>
-    </div>
+      <h2>Welcome to Author's Haven!</h2>
     </BrowserRouter>
   </Provider>
 );
-export default AppComp;
+export default App;
