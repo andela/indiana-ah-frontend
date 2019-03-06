da40e124ed1faf6505a068b715d79d0b143e5955
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-03-06 07:52:16 +0100

    chore: setup travis ci to run tests

--- _test_/first.test.js
+++ _test_/first.test.js
@@ -6,6 +6,6 @@ describe('first test', () => {
   const wrapper = shallow(<App />);
   it('should render the App', () => {
     expect(wrapper.length).toEqual(1);
-    expect(wrapper.find('div')).toHaveLength(1);
+    expect(wrapper.find('div')).toHaveLength;
   });
 });
