65c49516bf88b8b55ad1d579c7bf6461723f5e0f
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-03-07 16:20:48 +0100

    feat: add styling to the component

--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Footer from './common/footer.jsx';
 
 const IndexPage = () => (
-  <React.Fragment>
+  <Fragment>
     <div className='container'>
       <h2>Welcome to Author's Haven!</h2>
     </div>
     <Footer />
-  </React.Fragment>
+  </Fragment>
 );
 
 export default IndexPage;
--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => (
   <div className='footer'>
     <div className='container'>
       <Row>
-        <Col className='col-md-5 col-sm-3 pl-0'>
+        <Col className='col-lg-4 col-md-5 col-sm-4 col-3 d-none d-sm-block pl-0 align-self-center'>
           <div className='d-flex'>
-            <SVG src='../src/assets/images/svg/logo2.svg' className='logo2'></SVG>
-            <SVG src='../src/assets/images/svg/logo.svg' className='d-block logo-text'></SVG>
+            <SVG src='../src/assets/images/svg/logo2.svg' className='feather'></SVG>
+            <SVG src='../src/assets/images/svg/logo.svg' className='d-block app-logo'></SVG>
           </div>
         </Col>
-        <Col>
+        <Col className='d-flex align-items-center flex-column'>
           <h2 className='mb-5'>Help</h2>
           <Link to='#'>
             <h4 className='mb-4'>Profile</h4>
@@ -22,7 +22,7 @@ const Footer = () => (
             <h4>Blog</h4>
           </Link>
         </Col>
-        <Col>
+        <Col className='d-flex align-items-center flex-column'>
           <h2 className='mb-5'>Contact Us</h2>
           <Link to='#'>
             <h4 className='mb-4'>Faqs</h4>
@@ -31,14 +31,14 @@ const Footer = () => (
             <h4>Contact us</h4>
           </Link>
         </Col>
-        <Col>
+        <Col className='d-flex align-items-center flex-column'>
           <h2 className='mb-4'>Social Media</h2>
           <div className='d-flex justify-evenly w-75 mb-4'>
             <Link to='#'>
-              <SVG src='../src/assets/images/svg/facebook.svg' className='mr-5'></SVG>
+              <SVG src='../src/assets/images/svg/facebook.svg' className='mr-5 d-block logo2'></SVG>
             </Link>
             <Link to='#'>
-              <SVG src='../src/assets/images/svg/twitter.svg' className='d-block logo-text'></SVG>
+              <SVG src='../src/assets/images/svg/twitter.svg' className='d-block logo2'></SVG>
             </Link>
           </div>
           <div className='d-flex justify-content-between w-75'>
