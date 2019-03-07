58f78ed3427871d744ac690b5b06010328c3c7a6
bobbylemm <chidozienwoga@gmail.com>
2019-03-07 18:22:00 +0100

    feat: create navbar component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
+        <NavBar />
         <Route path="/" component={IndexPage} exact />
       </Switch>
     </BrowserRouter>
