ae3aa2bfc85e3c5aeddb654c3fa386e1409a1d9b
bobbylemm <chidozienwoga@gmail.com>
2019-03-07 18:22:00 +0100

    feat: create navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import IndexPage from './components/IndexPage.jsx';
+import React, { Fragment } from 'react';
+import NavBar from './components/common/Navbar.jsx';
 
 const App = () => (
   <Provider store={store}>
