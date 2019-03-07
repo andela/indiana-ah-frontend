fdaca94bb489649442920661d76bc6f419ab7d46
omenkish <omenkish@gmail.com>
2019-03-07 12:54:29 +0100

    Feat: create styled-component input

--- src/styles/common/input.jsx
+++ src/styles/common/input.jsx
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
 // Create an Input component that'll render an <input> tag with some styles
 const Input = styled.input`
-  padding: 0.5em;
-  margin: 0.3em;
-  color: ${props => props.inputColor || "palevioletred"};
-  height: 2.688rem;
+  padding: 0.8rem;
+  margin: 0.8rem;
+  height: 4.3rem;
   background: #FFFFFF;
-  border: 0.5px solid rgba(0, 0, 0, 0.4);
-  border-radius: 0.1875rem;
-  width: 23.19rem;
+  border: 0.05rem solid rgba(0, 0, 0, 0.4);
+  border-radius: 0.4rem;
+  width: ${props => props.inputWidth || "37.1rem"};
   box-sizing: border-box;
 `;
 
