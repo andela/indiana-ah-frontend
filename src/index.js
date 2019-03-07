cd5fa11a2aa8b9ffeb57c3405acad62c2dec3e57
bobbylemm <chidozienwoga@gmail.com>
2019-03-07 18:22:00 +0100

    feat: create navbar component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 import './styles/styles.scss';
-import Tag from './components/common/Tag.jsx';
 
 ReactDOM.render(
-  <React.Fragment>
-    <App />
-    <Tag />
-  </React.Fragment>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
