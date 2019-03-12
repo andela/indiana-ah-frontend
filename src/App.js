6d4d8db0b2d30f343c8550489bbd850c503f491d
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-03-12 11:00:12 +0100

    fix: delete unused packages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import IndexPage from './components/IndexPage.jsx';
-import ArticlesPage from './components/Articles';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route path="/articles" component={ArticlesPage} />
         <Route path="/" component={IndexPage} exact />
       </Switch>
     </BrowserRouter>
