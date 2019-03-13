8c23c427a61401231125c0f29a88517c283ba620
bobbylemm <chidozienwoga@gmail.com>
2019-03-13 17:16:23 +0100

    refactor: add social auth page

---
+++ src/components/SocialAuthPage.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+
+class SocialAuthPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>We are verifying your account.....</p>
+      </div>
+    );
+  }
+}
+
+export default SocialAuthPage;
