b08e71fe5dafd7c655b50476fecdfaab430d52ca
bobbylemm <chidozienwoga@gmail.com>
2019-03-14 09:20:39 +0100

    fix: fix conflict on branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from 'react';
-import NavBar from './components/common/Navbar.jsx';
-import './styles/styles.scss';
-import Footer from './components/common/footer.jsx';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import IndexPage from './components/IndexPage.jsx';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <NavBar />
         <Route path="/" component={IndexPage} exact />
       </Switch>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
 import './styles/styles.scss';
 
 ReactDOM.render(
