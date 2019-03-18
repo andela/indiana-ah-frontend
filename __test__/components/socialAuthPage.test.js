c5d2c76ef1c8640a56657af8c639c113e2f117f0
omenkish <omenkish@gmail.com>
2019-03-18 18:11:59 +0100

    test: write test for social auth reducer

--- __test__/components/socialAuthPage.test.js
+++ __test__/components/socialAuthPage.test.js
@@ -5,6 +5,8 @@ import thunk from 'redux-thunk';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import ConnectedSocialAuthPage from '../../src/components/SocialAuthPage.jsx';
+import reducer from '../../src/redux/reducers/authReducer';
+import { REGISTER_WITH_SM } from '../../src/redux/actions/actionTypes';
 
 const mockStore = configureStore([thunk]);
 const store = mockStore();
@@ -21,5 +23,24 @@ describe('Social auth page component', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('social auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      isLoading: false,
+      isAuthenticated: false,
+      isVerified: false,
+      error: ''
+    });
+  });
+  it('should authenticate the user', () => {
+    const successAction = {
+      type: REGISTER_WITH_SM,
+    };
+    expect(reducer({}, successAction)).toEqual({
+      isAuthenticated: true,
+    });
   });
 });
