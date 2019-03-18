ece5a04e1f699a6981b3bf98795db0518c3c9e74
omenkish <omenkish@gmail.com>
2019-03-18 15:45:02 +0100

    fix: rename SocialAuthPage.jsx

--- src/components/SocialAuthPage.jsx
+++ src/components/SocialAuthPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { registerWithSm } from '../redux/actions/authActions';
+import registerWithSm from '../redux/actions/authActions';
 
 class SocialAuthPage extends Component {
   componentDidMount() {
