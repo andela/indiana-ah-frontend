0f93c6a51e8dd0131434568687e8f8be7da26b6d
divinediscipline <divinediscipline4real@gmail.com>
2019-03-18 13:29:50 +0100

    remove sideNavigation component snapshot test

--- __test__/components/sideNav.test.js
+++ __test__/components/sideNav.test.js
@@ -6,7 +6,6 @@ import SideNav from '../../src/components/SideNav.jsx';
 describe('<SideNav/>', () => {
   it('should render the SideNav component with 3 NavLinks', () => {
     const wrapper = shallow(<SideNav/>);
-    expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('NavLink')).toHaveLength(3);
   });
 });
