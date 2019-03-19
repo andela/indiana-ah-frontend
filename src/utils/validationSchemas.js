6abcf5958c09be9dcab04c44a64b81659245e00f
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-03-19 13:36:07 +0100

    fix: modify input validation schemas

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,13 +1,13 @@
 export const signUpFormSchema = {
-  email: /[-.\w]{3,}@([\w-]+\.)+[\w-]{2,20}/,
-  username: /^(?=.*[a-zA-Z])\S{3,}$/,
-  password: /^(?=.*[0-9])(?=.*[a-zA-Z])([\S]{8,})$/
+  email: /^[a-z0-9][a-z0-9-]+[a-z0-9]+@([a-z]+\.)+[a-z]{2,20}$/,
+  username: /^([a-zA-Z0-9][\w-]+[a-zA-Z0-9]){1,}$/,
+  password: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]{8,})$/
 };
 
 export const validationMessages = {
   email: 'Please enter a valid email address',
   username:
-    'Username must be at least 3 characters long and must include at least an alphabet',
+    'Username must start and end with an alphabet and must be 3 characters long. Special characters are not allowed',
   password:
     'Password must contain alphabets and numbers and must be at least 8 characters long'
 };
