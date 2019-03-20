5e3d42a22fca37754ccd98641036452369cc5bc5
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-03-20 17:45:25 +0100

    refactor: refactor getUrl funtion

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,13 +3,10 @@ import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 
 export const getUrl = (hostName) => {
-  let url;
   if (hostName.includes('master')) {
-    url = 'https://indiana-ah-master.herokuapp.com/api/v1/';
-    return url;
+    return 'https://indiana-ah-master.herokuapp.com/api/v1/';
   }
-  url = 'https://indiana-ah-staging.herokuapp.com/api/v1/';
-  return url;
+  return 'https://indiana-ah-staging.herokuapp.com/api/v1/';
 };
 
 export const apiInstance = axios.create({
