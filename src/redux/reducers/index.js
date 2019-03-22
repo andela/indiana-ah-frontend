2c235211a7d820c6f3f032b505778b272c890e25
Akeem Balogun <balogunbiola101@gmail.com>
2019-03-22 10:58:30 +0100

    feat: reacfor axios api instance

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,22 +1,12 @@
 import { combineReducers } from 'redux';
-import { SIGN_OUT_USER } from '../actions/actionTypes';
 import authReducer from './authReducer';
 import articleReducer from './articleReducer';
 import userReducer from './userReducer';
 import bookmarkReducer from './bookmarkReducer';
 
-const appReducer = combineReducers({
+export default combineReducers({
   auth: authReducer,
   articles: articleReducer,
   user: userReducer,
   bookmarkedArticles: bookmarkReducer
 });
-
-const rootReducer = (state, action) => {
-  if (action.type === SIGN_OUT_USER) {
-    state = undefined;
-  }
-  return appReducer(state, action);
-};
-
-export default rootReducer;
