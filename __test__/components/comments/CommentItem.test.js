70e789df60ee8763700804eb24df1110e0465e99
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-04-03 14:58:30 +0100

    test: modify CommentItem test

--- __test__/components/comments/CommentItem.test.js
+++ __test__/components/comments/CommentItem.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import { CommentItem } from '../../../src/components/comment/CommentItem.jsx';
 import comments from '../../../__fixtures__/comments';
 
@@ -18,18 +19,18 @@ const props = {
 };
 
 test('It should render the comment item component', () => {
-  const wrapper = shallow(<CommentItem comment={comments[0]} {...props}/>);
-  expect(wrapper).toMatchSnapshot();
+  const tree = renderer.create(<CommentItem comment={comments[0]} {...props} />);
+  expect(tree).toMatchSnapshot();
 });
 
 test('It should render the comment item component', () => {
-  const wrapper = shallow(<CommentItem comment={comments[0]} {...props}/>);
+  const wrapper = shallow(<CommentItem comment={comments[0]} {...props} />);
   wrapper.setState({ modalIsOpen: true });
   expect(wrapper).toMatchSnapshot();
 });
 
 describe('Test CommentItem component', () => {
-  const wrapper = mount(<CommentItem comment={comments[0]} {...props}/>);
+  const wrapper = mount(<CommentItem comment={comments[0]} {...props} />);
   it('It should render the comment item component', () => {
     expect(wrapper.state('modalIsOpen')).toBe(false);
   });
@@ -37,10 +38,18 @@ describe('Test CommentItem component', () => {
   it('should simulate button clicks', () => {
     wrapper.setState({ modalIsOpen: true });
     const modal = wrapper.find('CustomModal');
-    expect(modal.find('h2').text()).toBe(' Are you sure you want to delete this comment?');
-    modal.find('button').at(0).simulate('click');
+    expect(modal.find('h2').text()).toBe(
+      ' Are you sure you want to delete this comment?'
+    );
+    modal
+      .find('button')
+      .at(0)
+      .simulate('click');
     expect(wrapper.state('modalIsOpen')).toBe(false);
-    modal.find('button').at(1).simulate('click');
+    modal
+      .find('button')
+      .at(1)
+      .simulate('click');
   });
 
   it('should find and simulate modal open button click', () => {
