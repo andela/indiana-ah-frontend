69c1ba24bb36b02b899347a030861d6643dfbfd0
Akeem Balogun <balogunbiola101@gmail.com>
2019-04-05 14:50:38 +0100

    feat: refactor follow hover component

--- src/components/common/userFollowSpan.js
+++ src/components/common/userFollowSpan.js
@@ -5,7 +5,6 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Button from 'react-bootstrap/Button';
 import { followOrUnfollow } from '../../redux/actions/userFollowActions';
 
-
 const UnfollowUser = (username, followUnFollowFunc) => {
   followUnFollowFunc(username, 'Unfollow');
 };
@@ -18,47 +17,55 @@ const renderTooltipUnfollow = props => (
       padding: '10px',
       color: 'white',
       borderRadius: 3,
-      ...props.style,
+      ...props.style
     }}
   >
-  <Button className='follow-btn'>Unfollow</Button>
+    <Button className="follow-btn">Unfollow</Button>
   </div>
 );
 
 const userFollowSpan = ({
-  userDetails: { username, imageUrl }, followOrUnfollow: followUnFollowFunc, visible
+  userDetails: { username, imageUrl },
+  followOrUnfollow: followUnFollowFunc,
+  visible
 }) => (
   <Fragment>
-<div className="col-lg-3 bg-suc mb-5 p-3" onClick={UnfollowUser.bind(this, username, followUnFollowFunc)}>
+    <div
+      className="col-lg-3 bg-suc mb-5 p-3"
+      onClick={UnfollowUser.bind(this, username, followUnFollowFunc)}
+    >
+      <div className="row">
+        <div className="col-md-4  vl">
+          <div className="d-inline-block">
+            <div className="user-image-container-f d-inline-block">
+              <img
+                src={imageUrl}
+                alt="John Doe"
+                className=" img-fluid mr-3 user-follow-img"
+              />
+            </div>
+          </div>
+        </div>
 
-
-<div className="row">
-  <div className="col-md-4  vl">
-    <div className="d-inline-block">
-    <div className='user-image-container-f d-inline-block'>
-<img src= {imageUrl} alt="John Doe" className=" img-fluid mr-3 user-follow-img"/>
-</div>
-</div>
-  </div>
-
-  <div className="col-md-8 vl">
-{
-  visible ? <OverlayTrigger
-  placement='top'
-  delay={{ show: 250, hide: 1300 }}
-  overlay={renderTooltipUnfollow}
->
-<div className="userfollow-name  d-inline-block">
-{username.replace(/\d{5,}/, '')}
-</div>
-</OverlayTrigger> : <div className="userfollow-name  d-inline-block">
-{username.replace(/\d{5,}/, '')}
-</div>
-}
-  </div>
-</div>
-  </div>
-
+        <div className="col-md-8 vl">
+          {visible === true ? (
+            <OverlayTrigger
+              placement="top"
+              delay={{ show: 250, hide: 1300 }}
+              overlay={renderTooltipUnfollow}
+            >
+              <div className="userfollow-name  d-inline-block">
+                {username.replace(/\d{5,}/, '')}
+              </div>
+            </OverlayTrigger>
+          ) : (
+            <div className="userfollow-name  d-inline-block">
+              {username.replace(/\d{5,}/, '')}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   </Fragment>
 );
 
@@ -68,9 +75,12 @@ userFollowSpan.propTypes = {
   userDetails: PropTypes.object,
   style: PropTypes.object,
   followOrUnfollow: PropTypes.func,
-  visible: PropTypes.bool,
+  visible: PropTypes.bool
 };
 
 export { userFollowSpan };
 
-export default connect(null, { followOrUnfollow })(userFollowSpan);
+export default connect(
+  null,
+  { followOrUnfollow }
+)(userFollowSpan);
